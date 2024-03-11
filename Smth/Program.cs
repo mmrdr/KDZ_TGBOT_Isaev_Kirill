b821c27ad9f7d3e6eacc4b7c36149fd27be83a9e
mmrdr <kialisaev@edu.hse.ru>
2024-03-11 21:33:33 +0300

    Добавить Nuget пакет Telegram.Bot

--- Smth/Program.cs
+++ Smth/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Smth
+﻿using Telegram.Bot;
+
+namespace Smth
 {
     internal class Program
     {
+        private static TelegramBotClient botClient;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            botClient = new TelegramBotClient("7067595343:AAFGej232xNIbzGt91dXZP-_rMgL0R8BHfQ") { Timeout = TimeSpan.FromSeconds(10) };
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine($"Bot id: {me.Id}, Bot name: {me.FirstName} ");
+            Console.ReadKey();
         }
     }
 }
